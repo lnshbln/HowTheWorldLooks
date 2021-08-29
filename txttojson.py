728f2958970fcd94d5e99ea0d9b7891c30cfa873
elesh <84030055+lnshbln@users.noreply.github.com>
2021-08-29 17:43:56 -0500

    Update txttojson.py

--- txttojson.py
+++ txttojson.py
@@ -5,7 +5,7 @@ lat = []
 lon = []
 color = []
 urls = []
-with open('/Users/elena/Documents/thesis/code/colors.txt') as csv_file:  # open vocabulary file
+with open('colors.txt') as csv_file:  # open vocabulary file
 
     csv_reader = csv.reader(csv_file, delimiter='\t')
     for row in csv_reader:
@@ -15,6 +15,6 @@ with open('/Users/elena/Documents/thesis/code/colors.txt') as csv_file:  # open 
         color.append(row[3])
         urls.append(row[4])
 
-file1 = open("/Users/elena/Documents/thesis/code/myfile.txt","w")
+file1 = open("myfile.txt","w")
 for i,j,k,c,url in zip(keywords,lat,lon,color,urls):
     file1.write('{'+'"keyword":'+'"'+i+'"'+',"lat":'+'"'+j+'"'+',"lon":'+'"'+k+'"'+',"color":'+'"'+c+'"'+','+'"media":'+'"'+url+'"'+'},'+'\n')
